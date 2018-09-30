59d3a5239e25385b3d06a081e3b16ceb55fbb27e
Balkrishna Meena <Balkrishna.Meena@in.ey.com>
2018-10-01 00:57:54 +0530

    deployed app on cloud foundry

--- config/main.js
+++ config/main.js
@@ -1,4 +1,5 @@
 module.exports = {
     port:process.env.PORT || 8888,
-    database:'mongodb://localhost:27017/fit_treat'
+   // database:'mongodb://localhost:27017/fit_treat'
+    database:'mongodb://fitTreat:Welcome12#@ds119343.mlab.com:19343/fit_treat'
 }
